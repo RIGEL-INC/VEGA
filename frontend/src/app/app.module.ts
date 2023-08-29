0536822108bcc7b4edbd838238b19a3910948d4d
Anton Sosin <defaultworking@gmail.com>
2023-08-30 01:50:25 +0300

    Added some libraries

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { RegistrationPageComponent } from './components/authentication/details/registration-page/registration-page.component';
 import { ProfilePageComponent } from './components/main/details/profile-page/profile-page.component';
 import { AuthLayoutComponent } from './components/authentication/details/UI/auth-layout/auth-layout.component';
+import {InputTextModule} from "primeng/inputtext";
+import {PaginatorModule} from "primeng/paginator";
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +26,12 @@ import { AuthLayoutComponent } from './components/authentication/details/UI/auth
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    PaginatorModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/authentication/details/registration-page/registration-page.component.ts
+++ frontend/src/app/components/authentication/details/registration-page/registration-page.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
   styleUrls: ['./registration-page.component.scss']
 })
-export class RegistrationPageComponent {
+export class RegistrationPageComponent implements OnInit{
+
 
   registerTitle = 'Register or ...'
   loginTitle = 'Login'
+
+  formGroup: FormGroup | undefined;
+  value: string | undefined;
+  value2: string | undefined;
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      text: new FormControl<string | null>(null)
+    });
+  }
 
 }
