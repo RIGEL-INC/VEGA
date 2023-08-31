f32185b5aaa48f7347639fe119fa21a8b8b08a62
Artem Krumin <trapper.programmer@gmail.com>
2023-08-31 12:34:57 +0300

    handle io.jsonwebtoken.security.SignatureException exception

--- backend/src/main/java/vega/com/backend/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/vega/com/backend/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package vega.com.backend.config;
 
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -31,31 +32,36 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        final String authHeader=request.getHeader("Authorization");
-        final String jwt;
-        final String username;
+        try {
+            final String authHeader=request.getHeader("Authorization");
+            final String jwt;
+            final String username;
 
-        if(authHeader==null || !authHeader.startsWith("Bearer ")){
-            filterChain.doFilter(request,response);
-            return;
-        }
-
-        jwt=authHeader.substring(7);
-        username=jwtService.extractUsername(jwt);
-        if(username != null && SecurityContextHolder.getContext().getAuthentication()==null){
-            PersonDetails personDetails=(PersonDetails) personDetailsService.loadUserByUsername(username);
-            if(jwtService.isTokenValid(jwt,personDetails)){
-                UsernamePasswordAuthenticationToken authToken=new UsernamePasswordAuthenticationToken(
-                        personDetails,
-                        null,
-                        personDetails.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+            if(authHeader==null || !authHeader.startsWith("Bearer ")){
+                filterChain.doFilter(request,response);
+                return;
             }
+
+            jwt=authHeader.substring(7);
+            username=jwtService.extractUsername(jwt);
+            if(username != null && SecurityContextHolder.getContext().getAuthentication()==null){
+                PersonDetails personDetails=(PersonDetails) personDetailsService.loadUserByUsername(username);
+                if(jwtService.isTokenValid(jwt,personDetails)){
+                    UsernamePasswordAuthenticationToken authToken=new UsernamePasswordAuthenticationToken(
+                            personDetails,
+                            null,
+                            personDetails.getAuthorities()
+                    );
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+            filterChain.doFilter(request,response);
+        }catch (SignatureException e){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid JWT signature");
         }
-        filterChain.doFilter(request,response);
     }
 }
