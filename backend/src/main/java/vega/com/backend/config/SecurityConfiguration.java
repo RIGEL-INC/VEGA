692e1985c24b9f671d9360e02868147968b35081
Artem Krumin <trapper.programmer@gmail.com>
2023-09-02 11:00:08 +0300

    back to boot 3.0.5

--- backend/src/main/java/vega/com/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/vega/com/backend/config/SecurityConfiguration.java
@@ -1,15 +1,17 @@
 package vega.com.backend.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -28,6 +30,9 @@ public class SecurityConfiguration {
 
         http
                 .cors(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers("/api/v1.0/auth/**",
                                         "/swagger-ui/**",
@@ -35,10 +40,10 @@ public class SecurityConfiguration {
                                         "/v3/api-docs").permitAll()
                         .anyRequest().authenticated()
                 )
-                .sessionManagement((sessionManagement) ->
-                        sessionManagement
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .httpBasic(Customizer.withDefaults())
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
